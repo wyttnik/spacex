fc3e711a3539f1cf1107cce3f44b76408282e1e6
wyttnik <81494616+wyttnik@users.noreply.github.com>
2022-11-22 03:13:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,20 +9,41 @@ function setup(){
     spaceX.launches().then(data=>{
         const listContainer = document.getElementById("listContainer")
         RenderLaunches(data, listContainer);
-        drawMap();
-    })
+        spaceX.launchpads().then(pads=>{
+            drawMap(pads);
+        });
+    });
 }
+
 function RenderLaunches(launches, container){
     const list = document.createElement("ul");
     launches.forEach(launch=>{
         const item = document.createElement("li");
         item.innerHTML = launch.name;
+        item.classList.add('launch');
+        item.setAttribute('launchpad',launch.launchpad);
         list.appendChild(item);
     })
     container.replaceChildren(list);
+    list.onmouseover = list.onmouseout = event => {
+        let e;
+        if (event.type === 'mouseover' & 
+            event.target.getAttribute('class') === 'launch') {
+                event.target.style.color = 'red';
+                e = document.getElementById(event.target.getAttribute('launchpad'))
+                e.parentNode.appendChild(e).setAttribute('fill','red');
+            }
+            
+        else if (event.type === 'mouseout' & 
+            event.target.getAttribute('class') === 'launch') {
+                event.target.style.color = '';
+                e = document.getElementById(event.target.getAttribute('launchpad'))
+                e.parentNode.appendChild(e).setAttribute('fill','blue');
+            }
+    };
 }
 
-function drawMap(){
+function drawMap(pads){
     const width = 640;
     const height = 480;
     const margin = {top: 20, right: 10, bottom: 40, left: 100};
@@ -36,17 +57,42 @@ function drawMap(){
         .scale(70)
         .center([0,20])
         .translate([width / 2 - margin.left, height / 2]);
+    var featureCollection = {
+        features: pads.map(d=> {
+            return {
+                'type':'Feature',
+                'geometry':{
+                    'type':'Point',
+                    'coordinates':[d.longitude,d.latitude]
+                },
+                'properties': { 'id':d.id }
+            }
+        })
+    };
+
     svg.append("g")
-        .selectAll("path")
-        .data(Geo.features)
-        .enter()
-        .append("path")
-        .attr("class", "topo")
-        .attr("d", d3.geoPath()
+    .selectAll("path")
+    .data(Geo.features)
+    .enter()
+    .append("path")
+    .attr("class", "topo")
+    .attr("d", d3.geoPath()
+        .projection(projection)
+    )
+    .attr("fill", function (d) {
+        return d3.color();
+    })
+    .style("opacity", .7);
+
+    svg.append("g")
+    .attr('class','launchpads')
+    .selectAll("path")
+    .data(featureCollection.features)
+    .enter()
+    .append("path")
+    .attr('id',d=>d.properties.id)
+    .attr("d", d3.geoPath()
             .projection(projection)
         )
-        .attr("fill", function (d) {
-            return colorScale(0);
-        })
-        .style("opacity", .7)
+    .attr("fill", 'blue');
 }
